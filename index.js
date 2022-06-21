720064bf796bdb820066b12e98bf0441dad8c555
João Pedro P. Lima <84398388+JoaoPedroSH@users.noreply.github.com>
2022-06-21 17:29:17 -0300

    up finish

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-var cors = require('cors')
-var app = express()
+const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+//let urlencodedParser = bodyParser.urlencoded({ extended: false }); //para post
+
+/** Endpoints */
+const sites = require('./src/data/sites.json')
+const segmentos = require('./src/data/segmentos.json')
 const cupons = require('./src/data/cupons.json')
+const configs = require('./src/data/configs.json')
 
-app.use(cors())
 
-app.get('/api/cupons', function (req, res, next) {
+app.get('/configs', (req, res) => {
+  res.json(configs)
+}) 
+
+app.get('/sites', (req, res) => {
+  res.json(sites)
+}) 
+
+app.get('/segmentos', (req, res) => {
+  res.json(segmentos)
+}) 
+
+app.get('/cupons', (req, res) => {
   res.json(cupons)
 })
 
-app.listen(3000, function () {
-  console.log('API Online (port 3000)')
+
+app.listen(3300, function () {
+  console.log('API Online (port 3300)')
 })
